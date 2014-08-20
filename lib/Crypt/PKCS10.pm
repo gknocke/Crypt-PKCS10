ae9e66f74075fe87abbbbb304c47ae329ae1812f
washburnwi64 <gideonknocke@googlemail.com>
2014-08-20 21:53:00 +0200

    Version 1.0

--- lib/Crypt/PKCS10.pm
+++ lib/Crypt/PKCS10.pm
@@ -30,66 +30,66 @@ use MIME::Base64;
 
 our @EXPORT  = qw();
 our @ISA     = qw(Exporter);
-our $VERSION = 0.1;
+our $VERSION = 1.0;
 
 my %oids = (
-    '2.5.4.6'              => 'countryName',
-    '2.5.4.8'              => 'stateOrProvinceName',
-    '2.5.4.10'             => 'organizationName',
-    '2.5.4.11'             => 'organizationalUnitName',
-    '2.5.4.3'              => 'commonName',
-    '1.2.840.113549.1.9.1' => 'emailAddress',
-    '1.2.840.113549.1.9.2' => 'unstructuredName',
-    '1.2.840.113549.1.9.7' => 'challengePassword',
-    '1.2.840.113549.1.1.1' => 'RSA encryption',
-    '1.2.840.113549.1.1.5' => 'SHA1 with RSA encryption',
-    '1.2.840.113549.1.1.4' => 'MD5 with RSA encryption',
-    '1.2.840.113549.1.9.14' => 'extensionRequest',
-    '1.3.6.1.4.1.311.13.2.3' => 'OS_Version',
-    '1.3.6.1.4.1.311.13.2.2' => 'EnrollmentCSP',
-    '1.3.6.1.4.1.311.21.20' => 'ClientInformation',
-    '1.3.6.1.4.1.311.21.7' => 'certificateTemplate',
-    '2.5.29.37'            => 'EnhancedKeyUsage',
-    '2.5.29.15'            => 'KeyUsage',
-    '1.3.6.1.4.1.311.21.10' => 'ApplicationCertPolicies',
-    '2.5.29.14'             => 'SubjectKeyIdentifier',
-    '2.5.29.17'             => 'subjectAltName',
-    '1.3.6.1.4.1.311.20.2'  => 'certificateTemplateName',
+    '2.5.4.6'                       => 'countryName',
+    '2.5.4.8'                       => 'stateOrProvinceName',
+    '2.5.4.10'                      => 'organizationName',
+    '2.5.4.11'                      => 'organizationalUnitName',
+    '2.5.4.3'                       => 'commonName',
+    '1.2.840.113549.1.9.1'          => 'emailAddress',
+    '1.2.840.113549.1.9.2'          => 'unstructuredName',
+    '1.2.840.113549.1.9.7'          => 'challengePassword',
+    '1.2.840.113549.1.1.1'          => 'RSA encryption',
+    '1.2.840.113549.1.1.5'          => 'SHA1 with RSA encryption',
+    '1.2.840.113549.1.1.4'          => 'MD5 with RSA encryption',
+    '1.2.840.113549.1.9.14'         => 'extensionRequest',
+    '1.3.6.1.4.1.311.13.2.3'        => 'OS_Version',
+    '1.3.6.1.4.1.311.13.2.2'        => 'EnrollmentCSP',
+    '1.3.6.1.4.1.311.21.20'         => 'ClientInformation',
+    '1.3.6.1.4.1.311.21.7'          => 'certificateTemplate',
+    '2.5.29.37'                     => 'EnhancedKeyUsage',
+    '2.5.29.15'                     => 'KeyUsage',
+    '1.3.6.1.4.1.311.21.10'         => 'ApplicationCertPolicies',
+    '2.5.29.14'                     => 'SubjectKeyIdentifier',
+    '2.5.29.17'                     => 'subjectAltName',
+    '1.3.6.1.4.1.311.20.2'          => 'certificateTemplateName',
     #untested
-    '2.5.29.19'                     => 'Basic Constraints',#ASN
+    '2.5.29.19'                     => 'Basic Constraints',
     '1.2.840.10040.4.1'             => 'DSA',
     '1.2.840.10040.4.3'             => 'DSA with SHA1',
     '0.9.2342.19200300.100.1.25'    => 'domainComponent',
-    '2.5.4.7'                       => 'localityName', #ASN
+    '2.5.4.7'                       => 'localityName', 
     '1.2.840.113549.1.1.11'         => 'SHA-256 with RSA encryption',
     '1.2.840.113549.1.1.13'         => 'SHA-512 with RSA encryption',
     '1.2.840.113549.1.1.2'          => 'MD2 with RSA encryption',
-    '1.2.840.113549.1.9.15'         => 'SMIMECapabilities', #ASN
+    '1.2.840.113549.1.9.15'         => 'SMIMECapabilities', 
     '1.3.14.3.2.29'                 => 'SHA1 with RSA signature',
-    '1.3.6.1.4.1.311.13.1'          => 'RENEWAL_CERTIFICATE', #MS
-    '1.3.6.1.4.1.311.13.2.1'        => 'ENROLLMENT_NAME_VALUE_PAIR', #MS
-    '1.3.6.1.4.1.311.13.2.2'        => 'ENROLLMENT_CSP_PROVIDER', #MS
-    '1.3.6.1.4.1.311.2.1.14'        => 'CERT_EXTENSIONS', #MS
+    '1.3.6.1.4.1.311.13.1'          => 'RENEWAL_CERTIFICATE',
+    '1.3.6.1.4.1.311.13.2.1'        => 'ENROLLMENT_NAME_VALUE_PAIR',
+    '1.3.6.1.4.1.311.13.2.2'        => 'ENROLLMENT_CSP_PROVIDER',
+    '1.3.6.1.4.1.311.2.1.14'        => 'CERT_EXTENSIONS',
     '1.3.6.1.5.2.3.5'               => 'KDC Authentication',
     '1.3.6.1.5.5.7.9.5'             => 'countryOfResidence',
     '2.16.840.1.101.3.4.2.1'        => 'SHA-256',
-    '2.5.4.12'                      => 'Title', #ASN
-    '2.5.4.13'                      => 'Description', #ASN
-    '2.5.4.17'                      => 'postalCode', #ASN
-    '2.5.4.4'                       => 'Surname', #ASN
-    '2.5.4.41'                      => 'Name', #ASN
-    '2.5.4.42'                      => 'givenName', #ASN
-    '2.5.4.46'                      => 'dnQualifier', #ASN
-    '2.5.4.12'                      => 'serialNumber', #ASN    
+    '2.5.4.12'                      => 'Title', 
+    '2.5.4.13'                      => 'Description', 
+    '2.5.4.17'                      => 'postalCode', 
+    '2.5.4.4'                       => 'Surname', 
+    '2.5.4.41'                      => 'Name', 
+    '2.5.4.42'                      => 'givenName', 
+    '2.5.4.46'                      => 'dnQualifier', 
+    '2.5.4.12'                      => 'serialNumber',     
 );
 
 my %oid2extkeyusage = (
-                        '1.3.6.1.5.5.7.3.1' => 'serverAuth',
-                        '1.3.6.1.5.5.7.3.2' => 'clientAuth',
-                        '1.3.6.1.5.5.7.3.3' => 'codeSigning',
-                        '1.3.6.1.5.5.7.3.4' => 'emailProtection',
-                        '1.3.6.1.5.5.7.3.8' => 'timeStamping',
-                        '1.3.6.1.5.5.7.3.9' => 'OCSPSigning',
+                '1.3.6.1.5.5.7.3.1' => 'serverAuth',
+                '1.3.6.1.5.5.7.3.2' => 'clientAuth',
+                '1.3.6.1.5.5.7.3.3' => 'codeSigning',
+                '1.3.6.1.5.5.7.3.4' => 'emailProtection',
+                '1.3.6.1.5.5.7.3.8' => 'timeStamping',
+                '1.3.6.1.5.5.7.3.9' => 'OCSPSigning',
 );
 
 sub new {
@@ -98,11 +98,9 @@ sub new {
 
     my $parser = _init();
 
-    if($der =~ /^\W+\w+\s+\w+\s+\w+\W+\s(.*)\s+\W+.*$/s) { #if PEM, convert to DER
-        $der = decode_base64($1);
+    if($der =~ /^-----BEGIN\s(NEW\s)?CERTIFICATE\sREQUEST-----\s(.*)\s-----END\s(NEW\s)?CERTIFICATE\sREQUEST-----$/s) { #if PEM, convert to DER
+        $der = decode_base64($2);
     }
-    #comment in if you get parser errors
-    #asn_dump($der);
 
     use bytes;
     #some Requests may contain information outside of the regular ASN.1 structure. These parts need to be stripped of
@@ -168,16 +166,14 @@ sub _convert_attributes {
     foreach my $entry ( @{$typeandvalues} ) {
          if (defined $oids{ $entry->{'type'}}) {
             $entry->{'type'} = $oids{ $entry->{'type'} };
-            my $parser = _init($entry->{'type'}) or confess "Parser error: ", $entry->{'type'}, " needs entry in ASN.1 definition!";
+            my $parser = _init($entry->{'type'}) or confess "Parser error: ", $entry->{'type'}, " needs entry in ASN.1 definition";
 
-            if ($entry->{'type'} eq 'extensionRequest') { #extensionRequest need a new layer
-                #In case the DER representation of 'extensionRequest' is needed. works for each attribute.          
+            if ($entry->{'type'} eq 'extensionRequest') {
                 $entry->{'values'} = $self->_convert_extensionRequest($entry->{'values'}[0]);
             }
             else {
-                #maybe there can be more than one value, haven't seen jet. 
                 if($entry->{'values'}->[1]) {confess "Incomplete parsing of attribute type: ", $entry->{'type'};}
-                $entry->{'values'} = $parser->decode($entry->{'values'}->[0]) or confess ".. looks like damaged input";
+                $entry->{'values'} = $parser->decode($entry->{'values'}->[0]) or confess "Looks like damaged input";
             }
          }    
     }
@@ -195,8 +191,6 @@ sub _convert_extensionRequest {
             $entry->{'extnID'} = $oids{ $entry->{'extnID'} };
             my $parser = _init($entry->{'extnID'}) or confess "parser error: ", $entry->{'extnID'}, " needs entry in ASN.1 definition!";
             $entry->{'extnValue'} = $parser->decode($entry->{'extnValue'}) or confess $parser->error, ".. looks like damaged input";
-            
-            #extension specific mapping
             $entry->{'extnValue'} = $self->_mapExtensions($entry->{'extnID'}, $entry->{'extnValue'});
         }
     }
@@ -209,12 +203,11 @@ sub _mapExtensions {
     my $id =shift;
     my $value = shift;
     if ($id eq 'KeyUsage') {
-        #cannot see an easier way for this stupid task
         my $bit =  unpack('C*', @{$value}[0]); #get the decimal representation
-        my $length = int(log($bit) / log(2) + 1); # some algebra to get the bit length
+        my $length = int(log($bit) / log(2) + 1); #get its bit length
         my @usages = reverse(qw(digitalSignature nonRepudiation keyEncipherment dataEncipherment keyAgreement keyCertSign cRLSign encipherOnly decipherOnly));
-        my $shift = ($#usages + 1) - $length; # computes the area we dont need in @usages
-        $value = join ", ", @usages[ grep { $bit & (1 << $_ - $shift) } 0 .. $#usages ]; # ugly hack to transfer bitmap to barewords
+        my $shift = ($#usages + 1) - $length; # computes the unused area in @usages
+        $value = join ", ", @usages[ grep { $bit & (1 << $_ - $shift) } 0 .. $#usages ]; #transfer bitmap to barewords
     }
     if ($id eq 'EnhancedKeyUsage') {
         foreach (@{$value}) {
@@ -225,8 +218,8 @@ sub _mapExtensions {
         $value = (unpack "H*", $value);
     }
     if ($id eq 'ApplicationCertPolicies') {
-        foreach(@{$value}) {
-            $_->{'policyIdentifier'} = $oid2extkeyusage{$_->{'policyIdentifier'}} if(defined $oid2extkeyusage{$_->{'policyIdentifier'}});
+        foreach my $entry (@{$value}) {
+            $entry->{'policyIdentifier'} = $oid2extkeyusage{$entry->{'policyIdentifier'}} if(defined $oid2extkeyusage{$entry->{'policyIdentifier'}});
         }       
     }
     return $value
@@ -238,7 +231,7 @@ sub _convert_rdn {
 
     my $typeandvalue = shift;
     my %hash;
-    foreach my $entry ( @{$typeandvalue}) {
+    foreach my $entry ( @{$typeandvalue} ) {
         if (defined $oids{ $entry->[0]->{'type'}}) {
             $hash{ $oids{ $entry->[0]->{'type'} } } = $entry->[0]->{'value'};
         }
@@ -376,6 +369,9 @@ ASN1
     return $parsed;
 }
 
+###########################################################################
+# interface methods
+
 sub commonName {
     my $self = shift;
     return $self->{'certificationRequestInfo'}->{'subject'}->{'commonName'}
@@ -386,10 +382,6 @@ sub organizationalUnitName {
     my $self = shift;
     return $self->{'certificationRequestInfo'}->{'subject'}->{'organizationalUnitName'}->{'utf8String'} || '';
 }
-
-
-###########################################################################
-# interface methods
 
 sub emailAddress {
     my $self = shift;
@@ -409,7 +401,7 @@ sub countryName {
 
 sub version {
     my $self = shift;
-    my $v    = $self->{'certificationRequestInfo'}->{'version'};
+    my $v = $self->{'certificationRequestInfo'}->{'version'};
     return "v1" if $v == 0;
     return "v2" if $v == 1;
     return "v3" if $v == 2;
@@ -436,7 +428,7 @@ sub signature {
 }
 
 sub attributes {
-    my $self       = shift;
+    my $self = shift;
     my $attributes = $self->{'certificationRequestInfo'}->{'attributes'};
     my %hash = map { $_->{'type'} => $_->{'values'} }
         @{$attributes};
